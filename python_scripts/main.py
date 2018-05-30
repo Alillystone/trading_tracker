036e5539b847cda65563447e7aa6abc87ca37e10
Aidan Lillystone <lillystonea@gmail.com>
2018-05-30 23:04:48 +0100

    WIP Simple predicatability test
    Implemented predicatability response for a given company stock data, using rolling FFT function.

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -15,6 +15,7 @@ data = quandl.get_table('WIKI/PRICES',
                         date = { 'gte': '2018-01-01', 'lte': '2018-05-01' },
                         paginate=True)
 
+predictability_responses = []
 for company_tag in company_taglist:
     company_data = data[data.ticker == company_tag]
 
@@ -25,4 +26,27 @@ for company_tag in company_taglist:
     price_data = (company_data.close).values
     stock_data = np.column_stack((date_data, price_data))
 
-    print (stock_data)
+    num_items      = len(stock_data)
+    prev_idx = num_items - 1
+    next_idx = num_items - 2
+    buffer_width   = 5
+
+    response_accumulation = 0.0
+    while (next_idx >= buffer_width):
+
+        prev_stock_slice = price_data[prev_idx - buffer_width:prev_idx]
+        next_stock_slice = price_data[next_idx - buffer_width:next_idx]
+
+        prev_fft = np.fft.fft(prev_stock_slice)
+        next_fft = np.fft.fft(next_stock_slice)
+
+        prev_response = np.angle(prev_fft)
+        next_response = np.angle(next_fft)
+
+        predictability_response  = np.sum(np.abs(prev_response - 
+                                                 next_response))
+        response_accumulation += predictability_response
+        prev_idx -= 1
+        next_idx -= 1
+    
+    predictability_responses.append((company_tag, predictability_response))
