59c87a0a4f18e2d06114e05bcd2ee2d09754d782
Aidan Lillystone <lillystonea@gmail.com>
2018-06-19 23:15:06 +0100

    TDY
    Tidying code by creating classes and moving functions into classes.

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -1,5 +1,6 @@
 import quandl
 import numpy as np
+import matplotlib.pyplot as plt
 from collections import namedtuple
 from requests import get
 from requests.exceptions import RequestException
@@ -8,10 +9,11 @@ from bs4 import BeautifulSoup
 
 class CompanyAnalysis:
 
-    def __init__(self, tag, confidence, predictability):
+    def __init__(self, tag, data, confidence, predictability):
         self.ticker = tag
         self.confidence = confidence
         self.predictability = predictability
+        self.data = data
 
     def write_properties(self):
         print ('--------------')
@@ -21,12 +23,25 @@ class CompanyAnalysis:
 
 APIKEY = "5V48hxsMxjK57i7gsNAT"
 quandl.ApiConfig.api_key = APIKEY
-company_taglist = ['AAPL', 'MSFT', 'WMT', 'FB', 'MU', 'BAC', 'F', 'GE', 'GM', 'CHK']
+company_taglist = ['AAPL', 
+                   'MSFT',
+                   'WMT',
+                   'FB', 
+                   'MU',
+                   'BAC', 
+                   'F',
+                   'GE',
+                   'GM',
+                   'CHK',
+                   'NVDA',
+                   'AMZN',
+                   'NFLX',
+                   'AMD']
 
 data = quandl.get_table('WIKI/PRICES', 
                         qopts = { 'columns': ['ticker', 'date', 'close'] }, 
                         ticker = company_taglist, 
-                        date = { 'gte': '2018-01-01', 'lte': '2018-05-01' },
+                        date = { 'gte': '2018-01-01', 'lte': '2018-06-10' },
                         paginate=True)
 
 company_analysis_data = []
@@ -116,15 +131,101 @@ sim_range = max(sim_list) - min(sim_list)
 dif_min   = min(dif_list)
 dif_range = max(dif_list) - min(dif_list)
 
+print (FFT_list)
+print (sim_list)
+print (dif_list)
+
 companies = []
+risk_list = []
 for company in company_analysis_data:
     predictability = (company.fft_response - FFT_min) / FFT_range
     sim_confidence = (company.simple_differential - sim_min) / sim_range
     dif_confidence = (company.accumulated_differential - dif_min) / dif_range
     confidence = (sim_confidence + dif_confidence) / 2
 
-    company_object = CompanyAnalysis(company.tag, confidence, predictability)
+    company_object = CompanyAnalysis(company.tag, company.data, confidence, predictability)
     companies.append(company_object)
 
-print (companies)
+    f, axes_array = plt.subplots(2, figsize=(20,10))
+    axes_array[0].plot(((company.data).close).values)
+    axes_array[1].bar([1,2], [confidence, predictability])
+    f.suptitle(company.tag)
+    output = company.tag + ".png"
+    f.savefig(output)
+
+    risk_percentage = ((predictability + 2 * confidence) / 3) * 100
+    risk_list.append((company.tag, risk_percentage))
+
+risk_array = np.asarray(risk_list)
+risk_array = np.sort(risk_array, axis=0)
+risk_array = np.flip(risk_array, 0)
+print (risk_array)
+
+class TraderAI:
+
+    class ScraperAI:
+
+    class AnalysisAI:
+
+        def __init__(start_date, end_date):
+            self.start_date = start_date
+            self.end_date   = end_date
+            company
+
+        def calculate_FFT(data):
+            num_items    = len(data)
+            prev_idx     = num_items - 1
+            next_idx     = num_items - 2
+            buffer_width = 5
+
+            response_accumulation = 0.0
+            while (next_idx >= buffer_width):
+
+                prev_stock_slice = data[prev_idx - buffer_width:prev_idx]
+                next_stock_slice = data[next_idx - buffer_width:next_idx]
+
+                prev_fft = np.fft.fft(prev_stock_slice)
+                next_fft = np.fft.fft(next_stock_slice)
+
+                prev_response = np.angle(prev_fft)
+                next_response = np.angle(next_fft)
+
+                predictability_response  = np.sum(np.abs(prev_response - 
+                                                         next_response))
+                response_accumulation += predictability_response
+                prev_idx -= 1
+                next_idx -= 1
+
+            return response_accumulation
+
+        def simple_diff(data):
+
+            return data[-1] - data[0]
+
+        def first_order_diff(temporal, data):
+
+        
+        def secnd_order_diff(temporal, data):
+
+
+    def __init__():
+        self.ticker_list = ['AAPL', 
+                            'WMT', 
+                            'FB', 
+                            'F',
+                            'MSFT', 
+                            'BAC', 
+                            'MU',
+                            'CHK',
+                            'GE',
+                            'GM',
+                            'NVDA',
+                            'AMZN',
+                            'NFLX',
+                            'AMD']
+
+    def run():
+
+
+
 
