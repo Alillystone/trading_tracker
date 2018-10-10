fb08637ff9b2dd57a6d7b23af550b14bd6753b36
Aidan Lillystone <lillystonea@gmail.com>
2018-10-10 18:56:55 +0100

    Neural Network
    Implemented neural network into main program flow.

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 def main():
 
-    # SentimentAnalyser = sentiment_analyser.SentimentAnalyser(min_neurons=1000,
-    #                                                          max_neurons=2000,
-    #                                                          num_hidden_layers=3)
-    # SentimentAnalyser.prepare_training_data()
-    # SentimentAnalyser.train_neural_network()
+    SentimentAnalyser = sentiment_analyser.SentimentAnalyser(min_neurons=1000,
+                                                             max_neurons=2000,
+                                                             num_hidden_layers=3)
+    SentimentAnalyser.prepare_training_data()
+    SentimentAnalyser.train_neural_network()
     
     TRADER = trader.TraderAI()
     TRADER.initialise()
